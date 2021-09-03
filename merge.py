eab0db76dc6a1f563dd17b307bac03be8efca179
rayray2002 <rayray2002.huang@gmail.com>
2021-09-03 21:27:06 +0800

    popularity added

--- merge.py
+++ merge.py
@@ -1,21 +1,27 @@
 import pandas as pd
 import glob
 
-df_new = pd.read_csv('./save_csv/popularity.csv')
 
-for file in glob.iglob('./save_csv/' + '**/*' + '.csv', recursive=True):
-    if str(file) == './save_csv/popularity.csv':
-        continue
-    print(file)
-    df_old = pd.read_csv(file)
-    df_old['流水號'] = df_old['流水號'].fillna(0).astype(int)
-    # print(df_old.head())
+def main():
+    df_new = pd.read_csv('./save_csv/popularity.csv')
 
-    df_new = df_new.astype(int)
+    for file in glob.iglob('./save_csv/' + '**/*' + '.csv', recursive=True):
+        if str(file) == './save_csv/popularity.csv':
+            continue
+        print(file)
+        df_old = pd.read_csv(file)
+        df_old['流水號'] = df_old['流水號'].fillna(0).astype(str)
+        # print(df_old.head())
 
-    df = pd.merge(df_old, df_new, on='流水號', how='left')
-    df = df.drop_duplicates(subset=['流水號'], keep='last')
-    print(df)
-    file_name = file.split('/')[-1]
-    df = df.sort_values(by=['登記人數'], ascending=False)
-    df.to_csv(f'./new_csv/{file_name}', encoding='utf-8-sig', index=False)
+        df_new = df_new.astype(str)
+
+        df = pd.merge(df_old, df_new, on='流水號', how='left')
+        df = df.drop_duplicates(subset=['流水號'], keep='last')
+        print(df)
+        file_name = file.split('/')[-1]
+        df = df.sort_values(by=['登記人數'], ascending=False)
+        df.to_csv(f'./new_csv/{file_name}', encoding='utf-8-sig', index=False)
+
+
+if __name__ == '__main__':
+    main()
